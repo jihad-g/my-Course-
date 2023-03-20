e168694b2c4fd05860ca1fd45ff1d4d4a4cb5671
mohammed <mo.abdulbaset@gmail.com>
2023-03-20 14:43:47 +0200

    add sqr function with validation

--- firstProjCalculator.php
+++ firstProjCalculator.php
@@ -11,7 +11,7 @@ function calculat($num1,$num2,$sign)
 {
   
     validation($num1,$num2,$sign);
-    $func=[ '+'=>'ADD','-'=>'MINUS','*'=>'multiply','/'=>'division' ];
+    $func=getAvlibaleOpartion();
     $f=$func[$sign];
    return  call_user_func($f,$num1,$num2);
    // AfterExcution();
@@ -23,6 +23,8 @@ function validation($num1,$num2,$sign)
 isnumrical($num1,$num2);
 oparationNotFound($sign);
 canNotDivideByZero($num2 ,$sign);
+sqrMustBePostive($num1,$sign);
+
 }
 function isnumrical($num1,$num2)
 {
@@ -31,9 +33,13 @@ function isnumrical($num1,$num2)
 
      
 }
+function getAvlibaleOpartion()
+{
+    return [ '+'=>'ADD','-'=>'MINUS','*'=>'multiply','/'=>'division','sqr'=>'sqr' ];
+}
 function oparationNotFound($sign)
 {
-    $func=[ '+'=>'ADD','-'=>'MINUS','*'=>'multiply','/'=>'division' ];
+    $func=getAvlibaleOpartion();
   
     if (isset($func[$sign])) return ;
     throw new Exception('Opration not found');
@@ -43,6 +49,14 @@ function canNotDivideByZero($num2,$sign)
     if ($num2 == 0 && $sign == "/")  
 {throw new Exception("can't divide by zero"); }
 return ;
+}
+function sqrMustBePostive($num1,$sign){
+    if ($num1 < 0 && $sign == "sqr")  
+    {throw new Exception("num1 must be more than zero"); }
+}
+function sqr($num1)
+{
+    return sqr($num1);
 }
 function excuteValidation($validations)
 {
