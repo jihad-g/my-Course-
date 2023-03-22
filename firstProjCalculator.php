6d17d8670fa12510780d953d477adc7ac155b4f7
jihad <jihadjamal221@gmail.com>
2023-03-22 11:11:10 +0200

    add it a circle ,twice and cubeRoot functions>>

--- firstProjCalculator.php
+++ firstProjCalculator.php
@@ -6,7 +6,7 @@ function handel($errno, $errstr, $errfile, $errline)
 echo 'error';
 }
 */
-echo calculat(0,0,'/');
+echo calculat(30,0,'cubeRoot');
 function calculat($num1,$num2,$sign)
 {
   
@@ -24,7 +24,8 @@ isnumrical($num1,$num2);
 oparationNotFound($sign);
 canNotDivideByZero($num2 ,$sign);
 sqrMustBePostive($num1,$sign);
-
+rootMustBePostive($num1,$sign);
+rMustBePostive($num1,$sign);
 }
 function isnumrical($num1,$num2)
 {
@@ -35,7 +36,7 @@ function isnumrical($num1,$num2)
 }
 function getAvlibaleOpartion()
 {
-    return [ '+'=>'ADD','-'=>'MINUS','*'=>'multiply','/'=>'division','sqr'=>'sqr' ];
+    return [ '+'=>'ADD','-'=>'MINUS','*'=>'multiply','/'=>'division','sqr'=>'sqr','cubeRoot'=>'cubeRoot','twice'=>'twice','circle'=>'circle' ];
 }
 function oparationNotFound($sign)
 {
@@ -53,6 +54,22 @@ return ;
 function sqrMustBePostive($num1,$sign){
     if ($num1 < 0 && $sign == "sqr")  
     {throw new Exception("num1 must be more than zero"); }
+}
+function rootMustBePostive($num1,$sign){
+    if ($num1 < 0 && $sign == "cubeRoot")  
+    {throw new Exception("num1 must be more than zero"); }
+}
+function rMustBePostive($num1,$sign){
+    if ($num1 < 0 && $sign == "circle")  
+    {throw new Exception("num1 must be more than zero"); }
+}
+function circle($num1)
+{
+    return circle($num1*3.14);
+}
+function cubeRoot($num1)
+{
+    return cubeRoot($num1*0.3);
 }
 function sqr($num1)
 {
@@ -77,6 +94,10 @@ function multiply ($num1,$num2)
 function division($num1,$num2)
 {
     return $num1/$num2; 
+}
+function twice($num1)
+{
+    return twice($num1*$num1);
 }
 
 function unkown()
